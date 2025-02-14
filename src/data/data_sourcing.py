67f0516cd93ddeff05d2c897caa46385dc0e4781
AdityaRoot <adityavmangla@gmail.com>
2025-02-14 20:18:18 +0800

    Updating with notebooks from hftbacktest library

--- src/data/data_sourcing.py
+++ src/data/data_sourcing.py
@@ -1,17 +1,13 @@
 from tardis_dev import datasets
 
 datasets.download(
-    exchange="bybit",
+    exchange="binance",
     data_types=[
         "incremental_book_L2",
         "trades",
-        "quotes",
-        "derivative_ticker",
-        "book_snapshot_25",
-        "liquidations"
     ],
     from_date="2024-06-01",
     to_date="2024-06-02",
-    symbols=["1000BEERUSDT"],
+    symbols=["BTCUSDT"],
     # api_key="YOUR API KEY (optionally)",
 )
